1270230b365db2a0c5f4c1839bbc2c3e898fb2b5
3A <524019351@qq.com>
2018-10-20 00:42:58 +0800

    scope兼容一下目前版本Variable的构造函数

--- scope.h
+++ scope.h
@@ -10,6 +10,7 @@ public:
     map<string,Function*> functionList;
     vector<Scope*> sonScope;
 
-    void addValue(string name) {this->variableList[name]=new Variable;} //先插进去后赋值
-    void addFunction(string name,Function* fun) {this->functionList[name]=fun;} //这个Function必须在外面先new好，因为参数比较多
+    void addValue(string name) {this->variableList[name]=new Variable();}
+    void addValue(string name,Variable* var) {this->variableList[name]=var;}
+    void addFunction(string name,Function* fun) {this->functionList[name]=fun;}
 };
